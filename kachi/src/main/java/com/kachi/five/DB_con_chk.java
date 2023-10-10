0fb065d31c7ee2515a746176af79c46c3dac167b
JangMinSeok <alstjr1921@naver.com>
2023-10-10 19:20:55 +0900

    aws rds 연동

--- kachi/src/main/java/com/kachi/five/DB_con_chk.java
+++ kachi/src/main/java/com/kachi/five/DB_con_chk.java
@@ -13,7 +13,7 @@ public class DB_con_chk {
 	     Class.forName("com.mysql.cj.jdbc.Driver"); 
 
 	   
-	    java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gatchix2","root","user"); 
+	    java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://gatchix2.cxfabflfb25n.ap-northeast-2.rds.amazonaws.com:3306/gatchix2","root","whfwkr123"); 
 	    System.out.println(con);
 	    }
 }
